a5eb617c52871227c242e026fe64ec07cc0966be
Erick <erickkan2004@gmail.com>
2024-12-14 19:55:33 -0300

    creating markets

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,11 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { View, Text, Alert } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
+
 import { api } from "@/services/api";
+import { colors, fontFamily } from "@/styles/theme";
+
 import { Categories, CategoriesProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+import { router } from "expo-router";
+
+type MarketsProps = PlaceProps & {
+    latitude: number
+    longitude: number
+}
+
+
+const currentLocation = {
+    latitude: -23.561187293883442,
+    longitude: -46.656451388116494
+}
 
 export default function Home(){
     const [categories, setCategories ] = useState<CategoriesProps>([]);
     const [category, setCategory] = useState("");
+    const [markets, setMarkets] = useState<MarketsProps[]>([])
+    const ref = useRef<MapView>(null)
 
     async function fetchCategories(){
         try {
@@ -18,14 +38,112 @@ export default function Home(){
         }
     }
 
+    async function fetchMarkets(){
+        try {
+            if(!category){
+                return
+            }
+
+            const {data} = await api.get("/markets/category/" + category);
+            setMarkets(data);
+            
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Locais", "Não foi possível carregar os locais")
+        }
+    }
+
     useEffect(() => {
         fetchCategories()
     }, [])
 
+    useEffect(() => {
+        fetchMarkets()
+    }, [category])
+
+    useEffect(() => {
+        if (ref.current) {
+          ref.current.animateCamera({
+            center: {
+              latitude: currentLocation.latitude,
+              longitude: currentLocation.longitude,
+            },
+            zoom: 16,
+          })
+        } 
+      }, [])
+
 
     return (
-        <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: "gray"}}>
             <Categories data={categories} onSelect={setCategory} selected={category} />
+
+            <MapView
+                style={{ flex: 1 }}
+                ref={ref}
+                initialRegion={{
+                    latitude: currentLocation.latitude,
+                    longitude: currentLocation.longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+            >
+                <Marker
+                 identifier="current"
+                 coordinate={{
+                    latitude: currentLocation.latitude,
+                    longitude: currentLocation.longitude,
+                 }}
+
+
+                 image={require("@/assets/location.png")}
+
+                
+                />
+
+                {
+                    markets.map(( item ) => (
+                        <Marker
+                            key={item.id}
+                            identifier={item.id}
+                            coordinate={{
+                                latitude: item.latitude,
+                                longitude: item.longitude
+                            }}
+                            
+                            image={require("@/assets/pin.png")}
+                        > 
+                            <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
+                                <View>
+                                    <Text 
+                                        style={{ 
+                                            fontSize: 14, 
+                                            color: colors.gray[600], 
+                                            fontFamily: fontFamily.medium,
+                                        }}
+                                    >
+                                            {item.name}
+                                        </Text>
+
+                                    <Text 
+                                        style={{ 
+                                            fontSize: 12, 
+                                            color: colors.gray[600], 
+                                            fontFamily: fontFamily.regular,
+                                        }}
+                                    >
+                                            {item.address}
+                                        </Text>
+                                </View>
+
+                            </Callout>   
+
+                        </Marker>
+                    ))
+                }
+            </MapView>
+
+            <Places data={markets} />
         </View>
     )
 }
--- src/components/place/index.tsx
+++ src/components/place/index.tsx
@@ -1,26 +1,39 @@
-import { TouchableOpacity, Image, View, Text } from "react-native";
+import { TouchableOpacity, Image, View, Text, TouchableOpacityProps } from "react-native";
 import { IconTicket } from "@tabler/icons-react-native";
 import { s } from "./styles"
 import { colors } from "@/styles/theme";
 
-export function Place(){
+export type PlaceProps = {
+    id: string
+    name: string
+    description: string
+    coupons: number
+    cover: string
+    address: string
+}
+
+type Props = TouchableOpacityProps & {
+    data: PlaceProps
+}
+
+export function Place({ data, ...rest}: Props){
     return (
-        <TouchableOpacity style={s.container}>
-            <Image style={s.image} />
+        <TouchableOpacity style={s.container} {...rest}>
+            <Image style={s.image} source={ {uri: data.cover} } />
 
             <View style={s.content}>
                 <Text style={s.name}>
-
+                    {data.name}
                 </Text>
 
-                <Text style={s.description}>
-
+                <Text style={s.description} numberOfLines={2}>
+                    {data.description}
                 </Text>
 
                 <View style={s.footer}>
                     <IconTicket size={16} color={colors.red.base} />
                     <Text style={s.tickets}>
-
+                        {data.coupons} cupons disponíveis
                     </Text>
                 </View>
             </View>
